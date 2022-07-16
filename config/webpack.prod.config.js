0226f4b9f4694ddf25880ba31adec86290657be2
unicornlee <unicornlee@dingtalk.com>
2022-07-16 09:29:42 +0800

    首次提交

---
+++ config/webpack.prod.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const OpenBrowserPlugin = require('open-browser-webpack4-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpackConfigBase = require('./webpack.base.config');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const webpackConfigDev = {
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin({
+      protectWebpackAssets: true,
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      title: 'React APP',
+      filename: 'index.html',
+      template: path.join(__dirname, '../src/index.html')
+    })
+  ],
+}
+
+module.exports = merge(webpackConfigBase, webpackConfigDev);
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="box">
+        Hello, React.
+        <span>I am Jack.</span>
+      </div>
+    );
+  }
+}
+
+export default App;
