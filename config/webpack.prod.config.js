f0b6aeeb02609c10475b235d2e7e83e7d81ffbde
unicornlee <unicornlee@dingtalk.com>
2022-07-24 14:26:28 +0800

    本地mock配置

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
-const OpenBrowserPlugin = require('open-browser-webpack4-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpackConfigBase = require('./webpack.base.config');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpackConfigBase = require('./webpack.base.config');
 
 const webpackConfigDev = {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin({
       protectWebpackAssets: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[fullhash:4].css',
     }),
     new HtmlWebpackPlugin({
       inject: 'body',
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,13 +1,24 @@
 import { Button } from 'antd';
 import React, { Component } from 'react';
+import axios from 'axios';
+import './style.scss';
 
 class IndexPage extends Component {
+  componentDidMount() {
+    axios.get('/ad/index/gray').then((res) => {
+      console.log('res: ', res.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
-      <div className="box">
-        Hello, React.
-        <span>Here is index page.</span>
-        <Button type="primary">按钮</Button>
+      <div className="index-page">
+        <div className="middle-box">
+          <span>Here is index page.</span>
+          <Button type="primary">按钮</Button>
+        </div>
       </div>
     );
   }
